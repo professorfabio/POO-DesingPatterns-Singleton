8d1b0c64e3868953d944ca07f914c6e1aa82a1b3
Fábio M. Costa <fmc@ufg.br>
2024-11-08 09:11:42 -0300

    Create TesteNumeracao.java

---
+++ TesteNumeracao.java
@@ -0,0 +1,8 @@
+public class TesteNumeracao { 
+   public static void main(String[] args) { 
+      for (int i = 0; i < 10; i++) { 
+         Numeracao num = new Numeracao(); 
+         System.out.println(num); 
+      } 
+   } 
+} 
