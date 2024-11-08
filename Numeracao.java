9864f591ef3e7522602590a177a2f002665585ab
Fábio M. Costa <fmc@ufg.br>
2024-11-08 09:11:12 -0300

    Create Numeracao.java

---
+++ Numeracao.java
@@ -0,0 +1,12 @@
+class Numeracao { 
+   private static int seq = 0; 
+   private int numero; 
+
+   public Numeracao() { 
+      numero = ++seq; 
+   } 
+
+   public String toString() { 
+      return "Numeração " + numero; 
+   } 
+} 
